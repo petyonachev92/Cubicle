44e61ad025c167b4fc75761382a37d469668689e
petyonachev92 <pyo@abv.bg>
2020-08-18 10:38:22 +0300

    feat: added all pages and fixed templates

--- config/express.js
+++ config/express.js
@@ -1,13 +1,20 @@
 const express = require('express');
 const handlebars = require('express-handlebars');
-const bodyParser = require('body-parser');
 
 module.exports = (app) => {
     
     //TODO: Setup the view engine
+    app.engine('.hbs', handlebars({
+        extname: '.hbs',
+        /*defaultLayout: 'main', 
+        layoutsDir: './views/layouts/',
+        partialsDir: './views/partials/'*/
+    }))
+    app.set('view engine', '.hbs')
 
     //TODO: Setup the body parser
 
     //TODO: Setup the static files
+    app.use('/static', express.static('static'))
 
 };
--- index.js
+++ index.js
@@ -1,9 +1,13 @@
+const router = require('./routes/routes');
+
 const env = process.env.NODE_ENV || 'development';
 
 const config = require('./config/config')[env];
 const app = require('express')();
+const indexRouter = require('./routes/routes')
 
 require('./config/express')(app);
-require('./config/routes')(app);
+
+app.use('/', indexRouter)
 
 app.listen(config.port, console.log(`Listening on port ${config.port}! Now its up to you...`));
