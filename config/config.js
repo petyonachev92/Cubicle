4fceb5a8d18e1f32bc2ac76d85daa775209f84d9
petyonachev92 <pyo@abv.bg>
2020-08-25 11:45:10 +0300

    db connection

--- config/config.js
+++ config/config.js
@@ -1,6 +1,7 @@
 module.exports = {
     development: {
-        port: process.env.PORT || 3000
+        port: process.env.PORT || 3000,
+        databaseUrl: `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.ymlsc.gcp.mongodb.net/${process.env.DB_DATABASE}?retryWrites=true&w=majority`
     },
     production: {}
 };
